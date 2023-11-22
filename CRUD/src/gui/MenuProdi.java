b8697ba7d2a68f42662602d2084d3c3e9e4c2175
Arfa Zaim Al Murtadlo <arfazaim0202@gmail.com>
2023-11-22 23:38:44 +0700

    crud
    mumet

--- CRUD/src/gui/MenuProdi.java
+++ CRUD/src/gui/MenuProdi.java
@@ -1,4 +1,112 @@
 package gui;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class MenuProdi {
+    private JPanel pnlProdi;
+    private JTextField tkode;
+    private JTextField tprodi;
+    private JButton savebtn;
+    private JButton updatebtn;
+    private JButton deletebtn;
+    private JButton cancelbtn;
+    private JTable table1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("MenuProdi");
+        frame.setContentPane(new MenuProdi().pnlProdi);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public MenuProdi() {
+        savebtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        updatebtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        deletebtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        cancelbtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+    public void createLayout(){
+        JFrame JFrame = new JFrame("MenuProdi");
+        JFrame.setContentPane(pnlProdi);
+        JFrame.pack();
+        JFrame.setLocationRelativeTo(null);
+        JFrame.setVisible(true);
+    }
+    Connection con;
+    private DefaultTableModel tabmode;
+    protected void tabelprodi(){
+        Object[] barisprodi = {"KODE PRODI","PRODI"};
+        tabmode = new DefaultTableModel(null, barisprodi);
+        tblprodi.setModel(tabmode);
+        String sql = "select * from tb_prodi";
+        try {
+            java.sql.Statement stat = con.createStatement();
+            ResultSet dataprodi = stat.executeQuery(sql);
+            while (dataprodi.next()){
+                String a = dataprodi.getString("kode_prodi");
+                String b = dataprodi.getString("nama_prodi");
+                String [] data={a,b};
+                tabmode.addRow(data);
+            }
+        }catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Data Gagal
+                    Tampil" +e);
+        }
+    }
+    public void kodeotomatis(){
+        try{
+            String sql="select right(kode_prodi,2) as kd_prod
+            from tb_prodi";
+            Statement stat=con.createStatement();
+            ResultSet res=stat.executeQuery(sql);
+            if(res.first()==false){
+                tkode.setText("PROD-01");
+            }else{
+                res.last();
+                int no=res.getInt(1)+1;
+                String cno=String.valueOf(no);
+                int pjg_cno=cno.length();
+                for(int i=0;i<2-pjg_cno;i++){
+                    cno= cno;
+                }
+                tkode.setText("PROD-0"+cno);
+            }
+        }catch(Exception e ){
+            System.out.println(e);
+        }
+    }
+    protected void kosongprodi() {
+        tkode.setText("");
+        tprodi.setText("");
+    }
+    public MenuProdi() {
+        Koneksi DBA = new Koneksi();
+        DBA.config();
+        con = DBA.con;
+        tabelprodi();
+        kodeotomatis();
+    }
 }
--- CRUD/src/gui/MenuUtama.java
+++ CRUD/src/gui/MenuUtama.java
@@ -1,12 +1,38 @@
 package gui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MenuUtama {
     private JPanel PnlRoot;
     private JButton akunmahasiswabtn;
     private JButton prodibtn;
     private JButton angkatanbtn;
+
+    public MenuUtama() {
+        akunmahasiswabtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                AkunMahasiswa Akunmhs = new AkunMahasiswa();
+                Akunmhs.createLayout();
+            }
+        });
+        prodibtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                MenuProdi Mprodi = new MenuProdi();
+                Mprodi.createLayout();
+            }
+        });
+        angkatanbtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                MenuAngkatan Mangkatan = new MenuAngkatan();
+                Mangkatan.createLayout();
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("MenuUtama");
