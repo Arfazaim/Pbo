59ce091997ec83bf538c6620d621ec0464819236
Arfa Zaim Al Murtadlo <arfazaim0202@gmail.com>
2023-11-25 09:57:57 +0700

    coba

--- CRUD/src/other/ComboProdi.java
+++ CRUD/src/other/ComboProdi.java
@@ -1,4 +1,14 @@
 package other;
-
 public class ComboProdi {
+    private String KdProdi, NmProdi;
+    public ComboProdi(String kdProdi, String nmProdi){
+        this.KdProdi = kdProdi;
+        this.NmProdi = nmProdi;
+    }
+    public String getKdProdi() {
+        return KdProdi;
+    }
+    public String getNmProdi() {
+        return NmProdi;
+    }
 }
